f89fe55481718ba4b36c25f57470170e1a62ccdd
Cameron Noemdo <cameronnoemdo@outlook.com>
2024-07-26 11:15:50 +0200

    Replaced hard-coded text with variables

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ def organiser(directory):
     org_path = Path().resolve().as_posix() + "/organised-files/"
     file_name = os.listdir(directory)
     folder_name = ['txt files', 'word doc files', 'spreadsheets']
+    txt_file_dir = "txt files/"
+    word_doc_dir = "word doc files/"
+    spreadsheet_dir = "spreadsheets/"
 
     # create directory for organised files
     if not os.path.exists(org_path):
@@ -20,13 +23,13 @@ def organiser(directory):
     # move each file type to its respective folder
     for i in range(0, len(file_name)):
         for j in file_name:
-            if ".txt" in j and not os.path.exists(org_path + "txt files/" + j):
-                shutil.copy(directory + j, org_path + "txt files/" + j)
+            if ".txt" in j and not os.path.exists(org_path + txt_file_dir + j):
+                shutil.copy(directory + j, org_path + txt_file_dir + j)
                 # ^ using .copy for demo purposes, should be replaced with .move
-            elif ".docx" in j and not os.path.exists(org_path + "word doc files/" + j):
-                shutil.copy(directory + j, org_path + "word doc files/" + j)
-            elif ".xlsx" in j and not os.path.exists(org_path + "spreadsheets/" + j):
-                shutil.copy(directory + j, org_path + "spreadsheets/" + j)
+            elif ".docx" in j and not os.path.exists(org_path + word_doc_dir + j):
+                shutil.copy(directory + j, org_path + word_doc_dir + j)
+            elif ".xlsx" in j and not os.path.exists(org_path + spreadsheet_dir + j):
+                shutil.copy(directory + j, org_path + spreadsheet_dir + j)
 
 
 directory_to_organise = Path().resolve().as_posix() + "/files/File/"
